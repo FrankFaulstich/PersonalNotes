427524e4f79bab2a90dfea266579f3b031238a79
Frank Faulstich <github@frank-faulstich.de>
2023-12-25 18:19:25 +0100

    Initial commit

---
+++ PersonalNotes.py
@@ -0,0 +1,45 @@
+import json
+import sys
+
+from InstallPyQt5 import installPyQt5
+installPyQt5()
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+class MyWindow(QMainWindow):
+    config = {}
+    notes = {}
+
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.ui = uic.loadUi("./MyApp.ui", self)
+        self.readConfig()
+        self.openNotes()
+
+    def readConfig(self):
+         # TODO: Was passiert, wenn die Config nicht gefunden wird?
+         with open('./config.json', 'r') as f:
+            self.config = json.load(f)
+            #print(self.config)
+
+    def openNotes(self):
+         file_name = self.config['NotesFolder'] + self.config['NotesFilename'] 
+         #print(file_name)
+         # TODO: Was passiert, wenn die Datei nicht gefunden wird?
+         with open(file_name, 'r') as f:
+            self.notes = json.load(f)
+            #print(self.notes)
+    
+
+def window():
+    app = QApplication(sys.argv)
+    with open('./styles.qss', 'r') as f:
+        style = f.read()
+        app.setStyleSheet(style)
+    win = MyWindow()
+    win.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+	window()
