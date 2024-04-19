4ef6ed3e402ab9bc1b45c4d143e8a069065bd387
Frank Faulstich <github@frank-faulstich.de>
2024-04-19 13:12:00 +0200

    Implement an about dialog Fixes #92

--- dialogs/about.py
+++ dialogs/about.py
@@ -1,2 +1,12 @@
+from PyQt6.QtWidgets import QMessageBox
+
 class About:
-    pass
+    
+    def __init__(self, version) -> None:
+        s = '<p>Persional Notes Version ' + version +  '</p>'
+        s = s + '<p>More Information:</p> '
+        s = s + '<a href=\"https://github.com/FrankFaulstich/PersonalNotes\">https://github.com/FrankFaulstich/PersonalNotes</a></p>'
+        msgBox = QMessageBox()
+        msgBox.setText(s)
+        msgBox.exec()
+
